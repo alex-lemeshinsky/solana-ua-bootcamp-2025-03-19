a2b8a84a677a7992f0f8bc71491b16fa72e7546f
Алексей Лемешинский <a.lemeshinsky@gmail.com>
2025-03-30 17:12:39 +0300

    rust generate keypair

---
+++ rust-exercises/src/generate_keypair.rs
@@ -0,0 +1,11 @@
+use solana_sdk::signature::{Keypair, Signer};
+
+pub fn generate_keypair() -> Keypair {
+    let keypair = Keypair::new();
+
+    println!("Public key: {}", keypair.pubkey());
+
+    println!("Secret key: {:?}", keypair.to_bytes());
+
+    return keypair;
+}
