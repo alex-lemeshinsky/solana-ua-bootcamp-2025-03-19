ae437cf7020152057ccffaf7d1316f4da0a0cd7d
Алексей Лемешинский <a.lemeshinsky@gmail.com>
2025-05-04 12:22:22 +0300

    practice 6.1 tokeOffer makeOffer code

--- practice-5/solana-bootcamp-empty/src/solana-service/program.ts
+++ practice-5/solana-bootcamp-empty/src/solana-service/program.ts
@@ -5,6 +5,13 @@ import { PublicKey } from "@solana/web3.js";
 import escrowIdl from "./escrow.json";
 import { Escrow } from "./idlType";
 import { config } from "./config";
+import { randomBytes } from "crypto";
+import {
+  getAssociatedTokenAddressSync,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+
+const TOKEN_PROGRAM = TOKEN_PROGRAM_ID;
 
 export class EscrowProgram {
   protected program: Program<Escrow>;
@@ -37,12 +44,66 @@ export class EscrowProgram {
     tokenAmountA: number,
     tokenAmountB: number
   ) {
+    const offerId = new BN(randomBytes(8));
+    const offerAddress = this.createOfferId(offerId);
+
+    const vault = getAssociatedTokenAddressSync(
+      tokenMintA,
+      offerAddress,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const makerTokenAccountA = getAssociatedTokenAddressSync(
+      tokenMintA,
+      this.wallet.publicKey,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const makerTokenAccountB = getAssociatedTokenAddressSync(
+      tokenMintB,
+      this.wallet.publicKey,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const accounts = {
+      maker: this.wallet.publicKey,
+      offer: offerAddress,
+      tokenMintA: tokenMintA,
+      tokenMintB: tokenMintB,
+      makerTokenAccountA: makerTokenAccountA,
+      makerTokenAccountB: makerTokenAccountB,
+      vault: vault,
+      tokenProgram: TOKEN_PROGRAM,
+    };
+
+    const txInstruction = await this.program.methods
+      .makeOffer(offerId, new BN(tokenAmountA), new BN(tokenAmountB))
+      .accounts(accounts)
+      .instruction();
+
+    const messageV0 = new web3.TransactionMessage({
+      payerKey: this.wallet.publicKey,
+      recentBlockhash: (await this.connection.getLatestBlockhash()).blockhash,
+      instructions: [txInstruction],
+    }).compileToV0Message();
+
+    const versionedTransaction = new web3.VersionedTransaction(messageV0);
+
+    if (!this.program.provider.sendAndConfirm) return;
+
     try {
-      console.log(tokenMintA, tokenMintB, tokenAmountA, tokenAmountB);
-      return null;
+      const response = await this.program.provider.sendAndConfirm(
+        versionedTransaction
+      );
+
+      if (!this.program.provider.publicKey) return;
+
+      return response;
     } catch (e) {
       console.log(e);
-      return null;
     }
   }
 
@@ -52,12 +113,72 @@ export class EscrowProgram {
     tokenMintA: PublicKey,
     tokenMintB: PublicKey
   ) {
+    const takerTokenAccountA = getAssociatedTokenAddressSync(
+      tokenMintA,
+      this.wallet.publicKey,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const takerTokenAccountB = getAssociatedTokenAddressSync(
+      tokenMintB,
+      this.wallet.publicKey,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const makerTokenAccountB = getAssociatedTokenAddressSync(
+      tokenMintB,
+      maker,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const valult = getAssociatedTokenAddressSync(
+      tokenMintA,
+      offer,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    const accounts = {
+      taker: this.wallet.publicKey,
+      maker: maker,
+      offer: offer,
+      tokenMintA: tokenMintA,
+      tokenMintB: tokenMintB,
+      takerTokenAccountA: takerTokenAccountA,
+      takerTokenAccountB: takerTokenAccountB,
+      makerTokenAccountB: makerTokenAccountB,
+      vault: valult,
+      tokenProgram: TOKEN_PROGRAM,
+    };
+
+    const txInstruction = await this.program.methods
+      .takeOffer()
+      .accounts(accounts)
+      .instruction();
+
+    const messageV0 = new web3.TransactionMessage({
+      payerKey: this.wallet.publicKey,
+      recentBlockhash: (await this.connection.getLatestBlockhash()).blockhash,
+      instructions: [txInstruction],
+    }).compileToV0Message();
+
+    const versionedTransaction = new web3.VersionedTransaction(messageV0);
+
+    if (!this.program.provider.sendAndConfirm) return;
+
     try {
-      console.log(maker, offer, tokenMintA, tokenMintB);
-      return null;
+      const response = await this.program.provider.sendAndConfirm(
+        versionedTransaction
+      );
+
+      if (!this.program.provider.publicKey) return;
+
+      return response;
     } catch (e) {
       console.log(e);
-      return null;
     }
   }
 }
